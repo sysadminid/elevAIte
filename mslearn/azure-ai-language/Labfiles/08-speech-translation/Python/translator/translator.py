a39cc33d35ec08fb700182b94226e113b3ab782c
Ahmad Sofyan <sofyan@btech.id>
2025-04-19 02:36:35 +0700

    updated for 08-speech-translation

--- mslearn/azure-ai-language/Labfiles/08-speech-translation/Python/translator/translator.py
+++ mslearn/azure-ai-language/Labfiles/08-speech-translation/Python/translator/translator.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import os
 
 # Import namespaces
+import azure.cognitiveservices.speech as speech_sdk
+from playsound import playsound
 
 
 def main():
@@ -16,10 +18,15 @@ def main():
         ai_region = os.getenv('SPEECH_REGION')
 
         # Configure translation
-
+        translation_config = speech_sdk.translation.SpeechTranslationConfig(ai_key, ai_region)
+        translation_config.speech_recognition_language = 'en-US'
+        translation_config.add_target_language('fr')
+        translation_config.add_target_language('es')
+        translation_config.add_target_language('hi')
+        print('Ready to translate from', translation_config.speech_recognition_language)
 
         # Configure speech
-
+        speech_config = speech_sdk.SpeechConfig(ai_key, ai_region)
 
         # Get user input
         targetLanguage = ''
@@ -29,7 +36,7 @@ def main():
                 Translate(targetLanguage)
             else:
                 targetLanguage = 'quit'
-                
+
 
     except Exception as ex:
         print(ex)
@@ -38,10 +45,27 @@ def Translate(targetLanguage):
     translation = ''
 
     # Translate speech
-
+    audio_file = 'station.wav'
+    playsound(audio_file)
+    audio_config = speech_sdk.AudioConfig(filename=audio_file)
+    translator = speech_sdk.translation.TranslationRecognizer(translation_config, audio_config=audio_config)
+    print('Getting speech from file...')
+    result = translator.recognize_once_async().get()
+    print(f'Translating "{result.text}"')
+    translation = result.translations[targetLanguage]
+    print(translation)
 
     # Synthesize translation
-
+    voices = {
+        'fr': 'fr-FR-HenriNeural',
+        'es': 'es-ES-ElviraNeural',
+        'hi': 'hi-IN-MadhurNeural'
+    }
+    speech_config.speech_synthesis_voice_name = voices.get(targetLanguage)
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
+    speak = speech_synthesizer.speak_text_async(translation).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
 
 
 if __name__ == "__main__":
