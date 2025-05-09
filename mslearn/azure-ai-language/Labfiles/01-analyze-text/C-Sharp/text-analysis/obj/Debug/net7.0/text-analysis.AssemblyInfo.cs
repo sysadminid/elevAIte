959393cffb800142f022caba3e710c7aa8377a08
Ahmad Sofyan <sofyan@btech.id>
2025-05-10 00:01:15 +0700

    update

--- mslearn/azure-ai-language/Labfiles/01-analyze-text/C-Sharp/text-analysis/obj/Debug/net7.0/text-analysis.AssemblyInfo.cs
+++ mslearn/azure-ai-language/Labfiles/01-analyze-text/C-Sharp/text-analysis/obj/Debug/net7.0/text-analysis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb914f65cd405713ec768cda11b0e2586820fc1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b6bd03712d506077bd096e44c5ca8ee71298d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
