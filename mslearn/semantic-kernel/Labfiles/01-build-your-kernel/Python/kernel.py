9d08df7ea34c67d7e6e2c2e0532211767888e311
Ahmad Sofyan <sofyan@btech.id>
2025-06-19 20:17:50 +0700

    upload semantic-kernel

---
+++ mslearn/semantic-kernel/Labfiles/01-build-your-kernel/Python/kernel.py
@@ -0,0 +1,38 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+
+# Import namespaces
+from semantic_kernel import Kernel
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+from semantic_kernel.functions.kernel_arguments import KernelArguments
+
+
+async def main():
+
+    load_dotenv()
+    # Set your values in the .env file
+    api_key = os.getenv("PROJECT_KEY")
+    endpoint = os.getenv("PROJECT_ENDPOINT")
+    deployment_name = os.getenv("DEPLOYMENT_NAME")
+
+
+    # Create a kernel with Azure OpenAI chat completion
+    kernel = Kernel()
+    chat_completion = AzureChatCompletion(
+        api_key=api_key,
+        endpoint=endpoint,
+        deployment_name=deployment_name
+    )
+    kernel.add_service(chat_completion)
+
+
+    # Test the chat completion service
+    response = await kernel.invoke_prompt(
+        "Give me a list of 10 breakfast foods with eggs and oats",
+        KernelArguments())
+    print("Assistant > \n\n" + str(response))
+
+
+if __name__ == "__main__":
+        asyncio.run(main())
---
+++ mslearn/semantic-kernel/Labfiles/02-run-prompts/C-sharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
+
+string filePath = Path.GetFullPath("appsettings.json");
+var config = new ConfigurationBuilder()
+    .AddJsonFile(filePath)
+    .Build();
+
+// Set your values in appsettings.json
+string apiKey = config["PROJECT_KEY"]!;
+string endpoint = config["PROJECT_ENDPOINT"]!;
+string deploymentName = config["DEPLOYMENT_NAME"]!;
+
+// Create a kernel with Azure OpenAI chat completion
+
+
+// Create the chat history
+
+
+// Create a semantic kernel prompt template
+
+
+// Render the prompt with arguments
+
+
+// Add the Semanitc Kernel prompt to the chat history and get the reply
+
+
+// Create a handlebars template
+
+
+// Render the Handlebars prompt with arguments
+
+
+// Add the Handlebars prompt to the chat history and get the reply
+
+
+// Get a follow-up prompt from the user
+
+
+// Add the user input to the chat history and get the reply
+
+
+async Task GetReply() {
+    // Get the reply from the chat completion service
+    
+}
